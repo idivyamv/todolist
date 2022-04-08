415615a0ec9395f2e4b9bb403ec462be0f241ad8
Divya <101665482+idivyamv@users.noreply.github.com>
2022-04-09 00:08:29 +0530

    change

--- js/validate.js
+++ js/validate.js
@@ -1,6 +1,5 @@
 
-function validate(event){ alert(test);
-     event.stopPropagation();
+function validate(event){ 
     // var mailformat    = '/^([A-Za-z0-9\.-]+)@([A-Za-z0-9\-]+).([a-z]{2,3})(\.[a-z]{2,3}?)$/';
      var Username        = document.getElementById('Username');
      var pswd            = document.getElementById('Password');   
@@ -14,7 +13,7 @@ function validate(event){ alert(test);
      }
      else {
         event.preventDefault();
-        return false;
+        Username = Username.value;
         userValidate(Username,pswd,userLogin);
         
      }
@@ -24,13 +23,14 @@ function validate(event){ alert(test);
      window.location.href = "todolist.html";
    }
    
-   function userValidate(Username, pswd, myCallback) {
+   function userValidate(Username, pswd, myCallback) {alert(Username+pswd);
     if(Username =='admin' && pswd =='12345')
          {   myCallback(Username);
             return false;
          }
      else {
-         document.getElementById("Error").innerHTML("Invalid username or Password.Please try again..")
+         document.getElementById("Error").innerHTML="Invalid username or Password.Please try again..";
+         document.getElementById("Error").classList.remove("d-none");
          return false;
      }
    }
